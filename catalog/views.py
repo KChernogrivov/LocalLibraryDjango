e23316c4ea289c3d9715f5e6574dd1e5881f7d10
Kirill Chernogrivov <kirill.2201@inbox.ru>
2022-10-01 11:35:06 +0700

    added forms update, delete, create for books

--- catalog/views.py
+++ catalog/views.py
@@ -113,3 +113,15 @@ class AuthorUpdate(UpdateView):
 class AuthorDelete(DeleteView):
     model = Author
     success_url = reverse_lazy('authors')
+
+class BookCreate(CreateView):
+    model = Book
+    fields = '__all__'
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = ["title", 'author', 'lang', 'summary', 'isbn', 'genre']
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = reverse_lazy('authors')
--- locallib/settings.py
+++ locallib/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = "qeqwdsDQA@#I4oLQ#;y4eU4123"
+SECRET_KEY = "qeqwdsDQA@#I4oLQ#464;y4eU4123"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
